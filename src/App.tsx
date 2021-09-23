9e20a2f1bf417179baba550c4f4b9c209a724a8c
Sonwanseo <sonwanseo@gmail.com>
2021-09-23 17:23:42 +0900

    create global style css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import GlobalStyle from "./styles/GlobalStyle";
+
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   );
 }
 
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  body, #root {
+    width: 100%;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
